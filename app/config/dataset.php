20f2eed8c76d01c148c0cddf7077d3a72f432d17
MardonioMelo <mardonio.quimico@gmail.com>
2021-03-15 00:13:42 -0300

    Novo metodo para cadastrar treinamentos

--- app/config/dataset.php
+++ app/config/dataset.php
@@ -3,15 +3,31 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 use App\Models\BotModel;
 
+# **********************
+# Cadastrar Novos Dados
+# **********************
+
 $BotModel = new BotModel;
 
-//Limpar tabela
+# Limpar tabela
 $BotModel->clearTable("chatbot", "app_bot");
 
-//Cadastrar novos dados
-echo "\n --- Início --- \n";
-$BotModel->createExemplesJsonFile('app_bot.json', true);
+# Inicio
+echo "\nInício --- " . date("H:i:s") . "\n";
+//sleep(1);
+
+# Buscar dados de um arquivo json
+# $BotModel->createExemplesJsonFile('app_bot.json', true);
+
+# Buscar dados de arquivos txt dentro de uma pasta
+$BotModel->createExemplesFolderTxt('treino/', true);
+//sleep(1);
+
+# Logs dos registros
 echo "\n" . $BotModel->getError() . "\n";
-echo "\n --- Fim --- \n\n";
+//sleep(1);
+
+# Fim
+echo "\nFim --- " . date("H:i:s") . "\n\n";
 
 exit;
