bc14307c7b31a61dc3ab3450fe163e8eeecddc1b
mardoniogr8 <mardonio.gr8@gmail.com>
2021-07-09 17:00:59 -0300

    update

--- src/db/chat_attendant.sql
+++ src/db/chat_attendant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 24-Jun-2021 às 19:26
+-- Tempo de geração: 09-Jul-2021 às 21:56
 -- Versão do servidor: 10.4.19-MariaDB
 -- versão do PHP: 7.4.19
 
